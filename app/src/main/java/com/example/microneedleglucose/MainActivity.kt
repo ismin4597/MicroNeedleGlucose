b89bce575b56740e3c1b24f7a5143eb6814bc9ae
insik <minis4597@naver.com>
2022-09-20 17:21:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/microneedleglucose/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.example.microneedleglucose
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.example.microneedleglucose.databinding.ActivityMainBinding
+import java.util.jar.Manifest
+
+val PERMISSION_CAMERA = 1000
+val REQUEST_CAMERA = 2000
+class MainActivity : AppCompatActivity() {
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+    private var realUri : Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonCamera.setOnClickListener(View.OnClickListener {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), PERMISSION_CAMERA)
+        })
+    }
+
+    fun requirePermissions(permissions : Array<String>, requestCode : Int){
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            permissionGranted(requestCode)
+        } else {
+            val isAllPermissionsGranted = permissions.all{checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED}
+            if (isAllPermissionsGranted) {
+                permissionGranted(requestCode)
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, requestCode)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(grantResults.all {it == PackageManager.PERMISSION_GRANTED}) {
+            permissionGranted(requestCode)
+        }else{
+            permissionDenied(requestCode)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun permissionGranted(requestCode : Int) {
+        when (requestCode){
+            PERMISSION_CAMERA -> openCamera()
+        }
+    }
+
+    private fun permissionDenied(requestCode: Int) {
+        when (requestCode){
+            PERMISSION_CAMERA -> Toast.makeText(
+                this,
+                "Camera permission is required to use camera.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun openCamera() {
+        val intent  = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        createImageUri(newFileName(), "image/jpg")?.let {uri ->
+            realUri = uri
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, realUri)
+            startActivityForResult(intent, REQUEST_CAMERA)
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun newFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+        return "$filename.jpg"
+    }
+
+    private fun createImageUri(filename : String, mimeType : String): Uri?{
+        var values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+        return this.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    }
+
+    override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == RESULT_OK){
+            when(requestCode){
+                REQUEST_CAMERA->{
+                    realUri?.let{ uri ->
+                        binding.imagePreview.setImageURI(uri)
+                    }
+                }
+            }
+        }
+    }
+}
