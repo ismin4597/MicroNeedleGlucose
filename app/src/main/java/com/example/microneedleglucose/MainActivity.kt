45a81744aef90413b865d3e9234ed2696380b6b4
insik <minis4597@naver.com>
2022-09-21 23:54:51 +0900

    Add - cropping image - blue filtered & get histogram

--- app/src/main/java/com/example/microneedleglucose/MainActivity.kt
+++ app/src/main/java/com/example/microneedleglucose/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.microneedleglucose
 
 import android.annotation.SuppressLint
+import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Color.blue
 import android.graphics.drawable.BitmapDrawable
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
@@ -17,9 +20,21 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.graphics.createBitmap
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.set
+import androidx.core.net.toUri
 import com.example.microneedleglucose.databinding.ActivityMainBinding
+import com.lyrebirdstudio.croppylib.Croppy
+import com.lyrebirdstudio.croppylib.main.CropRequest
+import com.lyrebirdstudio.croppylib.main.CroppyActivity
+import com.lyrebirdstudio.croppylib.main.CroppyTheme
+import com.lyrebirdstudio.croppylib.main.StorageType
+import com.lyrebirdstudio.croppylib.util.file.FileCreator
+import com.lyrebirdstudio.croppylib.util.file.FileExtension
+import com.lyrebirdstudio.croppylib.util.file.FileOperationRequest
 import java.io.ByteArrayOutputStream
+import java.io.File
 
 
 val PERMISSION_CAMERA = 1000
@@ -28,6 +43,7 @@ class MainActivity : AppCompatActivity() {
     private var mBinding : ActivityMainBinding? = null
     private val binding get() = mBinding!!
     private var realUri : Uri? = null
+    private var cropUri : Uri? = null
     private var drawable : BitmapDrawable? = null
     private var bitmap : Bitmap? = null
 
@@ -36,10 +52,48 @@ class MainActivity : AppCompatActivity() {
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requirePermissions(arrayOf(android.Manifest.permission.CAMERA), PERMISSION_CAMERA)
         binding.buttonCamera.setOnClickListener(View.OnClickListener {
-            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), PERMISSION_CAMERA)
+//            val cameraPreview = Intent(this@MainActivity, cameraActivity::class.java)
+//            startActivity(cameraPreview)
+            openCamera()
+        })
+        binding.buttonLoad.setOnClickListener(View.OnClickListener {
         })
 
+        binding.indicatorSeekbar.isClickable = false
+
+
+    }
+
+    private fun startCroppy(uri: Uri) {
+        val externalCropRequest = CropRequest.Auto(
+            sourceUri = uri,
+            requestCode = RC_CROP_IMAGE,
+            storageType = StorageType.CACHE
+        )
+
+        val croppedImageFile : File = FileCreator.createFile(
+            FileOperationRequest(StorageType.EXTERNAL, newFileName(), FileExtension.PNG),
+            application.applicationContext)
+        val destinationUri = croppedImageFile.toUri()
+
+        cropUri = destinationUri
+
+        val manualCropRequest = CropRequest.Manual(
+            sourceUri =  uri,
+            destinationUri = destinationUri!!,
+            requestCode = RC_CROP_IMAGE
+        )
+
+        val themeCropRequest = CropRequest.Manual(
+            sourceUri = uri,
+            destinationUri = destinationUri!!,
+            requestCode = RC_CROP_IMAGE,
+            croppyTheme = CroppyTheme(com.lyrebirdstudio.croppylib.R.color.blue)
+        )
+
+        Croppy.start(this, manualCropRequest)
     }
 
     fun requirePermissions(permissions : Array<String>, requestCode : Int){
@@ -55,7 +109,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -68,10 +121,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
     private fun permissionGranted(requestCode : Int) {
         when (requestCode){
-            PERMISSION_CAMERA -> openCamera()
+//            PERMISSION_CAMERA -> openCamera()
         }
     }
 
@@ -85,10 +137,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
     private fun openCamera() {
         val intent  = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        createImageUri(newFileName(), "image/bmp")?.let {uri ->
+        createImageUri(newFileName(), "image/png")?.let {uri ->
             realUri = uri
             intent.putExtra(MediaStore.EXTRA_OUTPUT, realUri)
             startActivityForResult(intent, REQUEST_CAMERA)
@@ -96,11 +147,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("SimpleDateFormat")
-    @RequiresApi(Build.VERSION_CODES.N)
     private fun newFileName() : String {
         val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
         val filename = sdf.format(System.currentTimeMillis())
-        return "$filename.bmp"
+        return "$filename.png"
     }
 
     private fun createImageUri(filename : String, mimeType : String): Uri?{
@@ -108,6 +158,14 @@ class MainActivity : AppCompatActivity() {
         values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
         values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
         return this.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    }
+
+    private fun createFileUri(filename: String, mimeType: String): Uri? {
+        var values = ContentValues()
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+        return contentResolver.insert(MediaStore.Files.getContentUri("external"), values);
     }
 
     override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?){
@@ -119,12 +177,51 @@ class MainActivity : AppCompatActivity() {
                     realUri?.let{ uri ->
                         binding.imagePreview.setImageURI(uri)
                         bitmap = binding.imagePreview.drawable.toBitmap()
+                        startCroppy(realUri!!)
                         Log.d("bitmapTest", "Width : ${bitmap!!.width.toString()} \tHeight : ${bitmap!!.height.toString()}")
                         Log.d("bitmapTest", "Pixel 1 : ${String.format("%x", bitmap!!.getPixel(0,0))}")
 
                     }
                 }
+                RC_CROP_IMAGE->{
+                    cropUri?.let{ uri ->
+                        binding.imagePreview.setImageURI(uri)
+                        bitmap = binding.imagePreview.drawable.toBitmap()
+                        Log.d("bitmapTest", "Width : ${bitmap!!.width.toString()} \tHeight : ${bitmap!!.height.toString()}")
+                        Log.d("bitmapTest", "Pixel 1 : ${String.format("%x", bitmap!!.getPixel(0,0))}")
+                        binding.imagePreview.setImageBitmap(blueFilter(bitmap!!))
+                        val histogram = getBlueHistogram(bitmap!!)
+                        Log.d("bitmapTest", "Histogram : " + histogram.toString())
+                    }
+                }
             }
         }
+
+
+    }
+    private fun blueFilter(inputBitmap : Bitmap) : Bitmap {
+        var outputBitmap = createBitmap(inputBitmap.width, inputBitmap.height, Bitmap.Config.ARGB_8888)
+        for(i in 0 until inputBitmap.height) {
+            for (j in 0 until inputBitmap.width){
+                outputBitmap[j,i] = 0xFF0000FF.toInt() and inputBitmap.getPixel(j,i).toInt()
+            }
+        }
+        return outputBitmap
+    }
+    private fun getBlueHistogram(inputBitmap: Bitmap) :MutableList<Int> {
+        val histogramBlue = MutableList<Int>(256) { _ -> 0 }
+        var tmp : Int = 0
+        for(i in 0 until inputBitmap.height) {
+            for(j in 0 until inputBitmap.width) {
+                tmp = 0x000000FF.toInt() and inputBitmap.getPixel(j,i).toInt()
+                histogramBlue[tmp]++
+            }
+        }
+        return histogramBlue
+    }
+
+
+    companion object {
+        private const val RC_CROP_IMAGE = 1001
     }
 }
